4bbe7c568a06841c9fb9b0a8d4a995ae45c20f10
Tomasz Bogusz <tbogusz@pc81.home>
2018-03-11 22:06:00 +0100

    Initial Commit

---
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ConcentrationThemeChooserViewController.swift
+//  Concentration
+//
+//  Created by Tomasz Bogusz on 05.03.2018.
+//  Copyright © 2018 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationThemeChooserViewController: UIViewController {
+    
+    let themes = [
+        "Sports":"ABCDEFGH",
+        "Animals":"IJKLMNOP",
+        "Faces":"QRSTUVWX"
+    ]
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "CHOOSE_THEME":
+                if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                    if let destination = segue.destination as? ConcentrationViewController {
+                        destination.theme = theme
+                    }
+                }
+            default:
+                break
+            }
+        }
+    }
+
+}
